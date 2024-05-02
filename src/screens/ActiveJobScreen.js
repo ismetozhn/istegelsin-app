b81a3a90008d3f86776a15ba57eab1be8018b3f0
Berkan <sukutberkan@gmail.com>
2024-05-02 18:25:42 +0300

    Kullanıcı OTP işlevi yapıldı.

--- src/screens/ActiveJobScreen.js
+++ src/screens/ActiveJobScreen.js
@@ -6,89 +6,86 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/core';
 
 export default function ActiveJobScreen() {
-    const [activeJob, setActiveJob] = useState(null);
-    const navigation = useNavigation();
-  
-    useEffect(() => {
-      const fetchActiveJob = async () => {
-        try {
-          // AsyncStorage'den userid'yi al
-          const userId = await readDataByKey(Keys.userid);
-  
-          // UserId'yi kullanarak API'den işleri getir
-          const response = await get(`https://ig.colaksoft.online/api/v1/JobApplication/ListJobsAcceptedByUser?is_user_accepted=true&userid=${userId}`, {}, {}, true);
-  
-          console.log("API Response:", response); // API yanıtını logla
-  
-          // API'den gelen verileri kontrol et ve işle
-          if (response.isSuccess && response.data.length > 0) {
-            // Örneğin, ilk kabul edilmiş işi alalım
-            const activeJobData = response.data[0];
-            setActiveJob(activeJobData);
-          } else {
-            console.log("Kullanıcının aktif bir işi bulunamadı.");
-          }
-        } catch (error) {
-          console.error("Aktif iş getirilirken bir hata oluştu:", error);
+  const [activeJob, setActiveJob] = useState(null);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const fetchActiveJob = async () => {
+      try {
+        // AsyncStorage'den userid'yi al
+        const userId = await readDataByKey(Keys.userid);
+
+        // UserId'yi kullanarak API'den işleri getir
+        const response = await get(`https://ig.colaksoft.online/api/v1/JobApplication/ListJobsAcceptedByUser?is_user_accepted=true&userid=${userId}`, {}, {}, true);
+
+        console.log("API Response:", response); // API yanıtını logla
+
+        // API'den gelen verileri kontrol et ve işle
+        if (response.isSuccess && response.data.length > 0) {
+          // Örneğin, ilk kabul edilmiş işi alalım
+          const activeJobData = response.data[0];
+          setActiveJob(activeJobData);
+        } else {
+          console.log("Kullanıcının aktif bir işi bulunamadı.");
         }
-      };
-  
-      fetchActiveJob();
-    }, []);
-
-    
-  
-  
-   
-    
-    
-    return (
-        <LinearGradient
-          colors={['#118dbf', '#3b5998', '#7d3cf2']}
-          style={{ flex: 1 }}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }}
-        >
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <View style={{ width: '90%', maxWidth: 400 }}>
-            <LinearGradient
-          colors={['#4c669f', '#3b5998', '#192f6a']}
-          style={{ padding: 16, borderRadius: 8, shadowOpacity: 0.3, shadowRadius: 10, elevation: 10 }}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 0 }}
-        >
-          {activeJob ? (
-            <LinearGradient
-              colors={['#4c669f', '#3b5998', '#192f6a']}
-              style={{ padding: 12, borderRadius: 8 }}
-              start={{ x: 0, y: 0 }}
-              end={{ x: 1, y: 0 }}
-            >
-              <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white', marginBottom:10 }}>Şirket: {activeJob.company_name}</Text>
-              <Text style={{ fontSize: 16, color: 'white',marginBottom:8 }}>Adres: {activeJob.adress}</Text>
-              <Text style={{ fontSize: 18, fontWeight: 'bold', color: 'white', marginBottom:8 }}>Pozisyon: {activeJob.title}</Text>
-              <Text style={{ fontSize: 16, color: 'white',marginBottom:8 }}>Başlangıç: {activeJob.start_at}</Text>
-              <Text style={{ fontSize: 16, color: 'white',marginBottom:8 }}>Bitiş: {activeJob.end_at}</Text>
-
-              <View className=" mb-3 mt-5">
-                <TouchableOpacity
-                    onPress={() => navigation.navigate('Otp')}
-                    className="py-3 bg-indigo-500 mx-7 rounded-xl ">
-                    <Text
-                        className="text-xl font-bold text-center text-gray-200 "
-                    >
-                       Onaylama & Değerlendirme
-                    </Text>
-                </TouchableOpacity>
-            </View>
-            </LinearGradient>
-          ) : (
-            <Text style={{ fontSize: 20, textAlign: 'center', fontWeight: '600', color: 'white' }}>Aktif iş bulunamadı.</Text>
-          )}
-        </LinearGradient>
-            </View>
-          </View>
-        </LinearGradient>
-      );
+      } catch (error) {
+        console.error("Aktif iş getirilirken bir hata oluştu:", error);
+      }
     };
-    
+
+    fetchActiveJob();
+  }, []);
+
+
+
+
+
+
+
+  return (
+    <LinearGradient
+      colors={['#118dbf', '#3b5998', '#7d3cf2']}
+      style={{ flex: 1 }}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ width: '90%', maxWidth: 400 }}>
+          <LinearGradient
+            colors={['#4c669f', '#3b5998', '#192f6a']}
+            style={{ padding: 16, borderRadius: 8, shadowOpacity: 0.3, shadowRadius: 10, elevation: 10 }}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+          >
+            {activeJob ? (
+              <LinearGradient
+                colors={['#4c669f', '#3b5998', '#192f6a']}
+                style={{ padding: 12, borderRadius: 8 }}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+              >
+                <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white', marginBottom: 10 }}>Şirket: {activeJob.company_name}</Text>
+                <Text style={{ fontSize: 16, color: 'white', marginBottom: 8 }}>Adres: {activeJob.adress}</Text>
+                <Text style={{ fontSize: 18, fontWeight: 'bold', color: 'white', marginBottom: 8 }}>Pozisyon: {activeJob.title}</Text>
+                <Text style={{ fontSize: 16, color: 'white', marginBottom: 8 }}>Başlangıç: {activeJob.start_at}</Text>
+                <Text style={{ fontSize: 16, color: 'white', marginBottom: 8 }}>Bitiş: {activeJob.end_at}</Text>
+
+                <View className=" mb-3 mt-5">
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate('Otp', { activeJobData: activeJob })}
+                    className="py-3 bg-indigo-500 mx-7 rounded-xl">
+                    <Text className="text-xl font-bold text-center text-gray-200">
+                      Onaylama & Değerlendirme
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </LinearGradient>
+            ) : (
+              <Text style={{ fontSize: 20, textAlign: 'center', fontWeight: '600', color: 'white' }}>Aktif iş bulunamadı.</Text>
+            )}
+          </LinearGradient>
+        </View>
+      </View>
+    </LinearGradient>
+  );
+};
--- src/screens/OtpUserScreen.js
+++ src/screens/OtpUserScreen.js
@@ -1,12 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import { get,add } from '../api/apiHelperDeneme'; // API yardımcı fonksiyonunu doğru dosyaya göre güncelleyin
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 export default function OtpUserScreen() {
-  return (
-    <View>
-      <Text>OtpUserScreen</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
+  const route = useRoute();
+  const activeJobData = route.params?.activeJobData; // ActiveJobScreen'den gelen verileri al
+  const [otp, setOtp] = useState('');
 
-const styles = StyleSheet.create({})
+  useEffect(() => {
+    fetchOtp(activeJobData, setOtp);
+  }, [activeJobData]); 
+
+  const fetchOtp = async (activeJobData, setOtp) => {
+    try {
+      const response = await get(`https://ig.colaksoft.online/api/v1/WorkAttendance?job_postingid=${activeJobData.job_postingid}&userid=${activeJobData.userid}`, {}, {}, true);
+      
+      // Response'dan OTP değerini al
+      const receivedOtp = response?.data?.otp;
+  
+      // OTP değerini state'e ayarla
+      setOtp(receivedOtp);
+    } catch (error) {
+      console.error('OTP alınırken hata oluştu:', error);
+    }
+  };
+  
+
+  // OtpUserScreen component
+  const handlePostButtonClick = async () => {
+    try {
+      const headers = {
+        'Content-Type': 'application/json-patch+json',
+      };
+
+      // activeJobData'dan gelen verileri istek verilerine ekle
+      const requestData = {
+        job_postingid: activeJobData.job_postingid,
+        userid: activeJobData.userid,
+        otp: otp, // Burada aldığımız OTP'yi kullanıyoruz
+        is_otp_verified: true,
+        created_at: new Date(), // Şu anki zamanı kullanarak created_at değerini oluştur
+      };
+
+      // API'ye istek gönder
+      const response = await add('WorkAttendance', requestData, headers, true);
+
+      console.log('API yanıtı:', response);
+
+      if (response && response.isSuccess) {
+        Alert.alert('Başarılı', 'İstek başarıyla tamamlandı.');
+      } else {
+        Alert.alert('Hata', 'İstek sırasında bir hata oluştu.');
+      }
+    } catch (e) {
+      console.error('Veri gönderme hatası:', e);
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ marginBottom: 20 }}>OTP: {otp}</Text>
+      <TouchableOpacity onPress={handlePostButtonClick} style={{ padding: 10, backgroundColor: '#4c669f', borderRadius: 8 }}>
+        <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold', textAlign: 'center' }}>OTP Gönder</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
